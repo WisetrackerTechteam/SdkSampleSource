084ed0a417f7e325e54e19d84102fa913687a69b
kimkunsoo <>
2022-08-10 18:15:53 +0900

    wisetracker sdk implements with nextJs

--- SampleNextJs/pages/index.js
+++ SampleNextJs/pages/index.js
@@ -1,6 +1,29 @@
-import Head from 'next/head'
+import React from "react"
+import Head from 'next/head' 
+import WDOTInitialization from "dop-website-sdk"
 
 export default function Home() {
+
+
+  /**
+   * WiseTracker SDK 초기화 
+   **/
+  React.useEffect(()=>{ 
+    window.WDOTInitialization.setConfig({
+        serviceNumber:102,
+        dotAccessToken:"7oGUQDmojFDLNqP+lj77clTq7SVpQ9/b/SgBCEyB9kL4kJcvn+XF++v4/oNUSZ0WI2nVYKDDckUSrcVnrkSWILdvUdow0PlKah3CJVVpj5wQEyafUSAxIjtZL292U4cs",
+        combackUserLimitDays:14,
+        dotEndPoint:"https://trk.analytics.wisetracker.co.kr/web/v1/dataRcv.do",
+        adClkEndPoint:"https://trk.analytics.wisetracker.co.kr/ldsys/v1/clickDataRcv.do",
+        adLandingEndPoint:"https://app.wisetracker.co.kr",
+        includeUrl:"localhost|192.168.0.3|wisetracker.co.kr|analytics.wisetracker.co.kr|sk7mobile.com",
+        excludeUrl:"",
+        referrerExpire:7
+    });
+    window.WDOTInitialization.init(); 
+  },[]); 
+
+
   return (
     <div className="container">
       <Head>
